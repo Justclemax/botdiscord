239234e6677bd120a6c2ed7fc6d83ccd941a78a9
Menclet <clementlonde23@gmail.com>
2023-11-12 17:02:10 +0000

    Oponai

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 
 load_dotenv()
@@ -9,9 +10,20 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+@client.event
+async def on_ready():
+    print(f" {client.user.name} est en ligne ")
+@client.event
+async def on_message(message : discord.message):
+    
+    if message.author.bot == True:
+        return
+    
+    if "hello" in message.content:
+        await message.channel.send('hello')
 
 
-
+    
 
 
 
---
+++ src/openai_api_key.py
@@ -0,0 +1,20 @@
+import openai
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+# Load API key from environment variable
+load_dotenv()
+ 
+
+client = OpenAI()
+
+response = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Who won the world series in 2020?"},
+    {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+    {"role": "user", "content": "Where was it played?"}
+  ]
+)
+print(response)
